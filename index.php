25064ec3b31310486773c4d4425b5a81aadbff79
Joao Feichas <developer@joaofeichas.com>
2019-11-10 10:25:56 -0300

    ENH - Implemented the admin page

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \JoaoFeichas\Page;
+use Slim\Slim;
+use JoaoFeichas\Page;
+use JoaoFeichas\PageAdmin;
 
 $app = new Slim();
 
@@ -15,6 +16,14 @@ $app->get('/', function() {
 
 	$page->setTpl("index");
 
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+	
 });
 
 $app->run();
