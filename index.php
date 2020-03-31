96399b74a0e052749c8aee89765203f5649e0bbe
Joao Feichas <developer@joaofeichas.com>
2020-03-31 06:08:12 -0300

    ENH - Site categorias

--- index.php
+++ index.php
@@ -266,4 +266,17 @@ $app->post("/admin/categories/:idcategory", function ($idcategory) {
 	exit;
 });
 
+$app->get("/categories/:idcategories", function ($idcategory) {
+	$category = new Category();
+
+	$category->get((int) $idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category' => $category->getValues(),
+		'products' => []
+	]);
+});
+
 $app->run();
