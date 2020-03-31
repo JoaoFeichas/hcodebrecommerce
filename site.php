c38032b563f161f2b9ee2223a07369c6526549a8
Joao Feichas <developer@joaofeichas.com>
2020-03-31 16:24:39 -0300

    110. Site - Produtos

--- site.php
+++ site.php
@@ -1,9 +1,14 @@
 <?php
 
+use JoaoFeichas\Model\Product;
 use JoaoFeichas\Page;
 
 $app->get('/', function () {
+    $products = Product::listAll();
+
     $page = new Page();
 
-    $page->setTpl("index");
+    $page->setTpl("index", [
+        'products' => Product::checkList($products)
+    ]);
 });
